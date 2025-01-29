113b1d36d91d54aca92b5e6ffcc94bc576d143c8
Parriego85 <parriego@yahoo.com>
2025-01-29 13:19:08 +0100

    segunda actualizacion

--- Encuesta_Pijam.py
+++ Encuesta_Pijam.py
@@ -14,30 +14,42 @@ img_malbec = "https://www.wearfigs.com/i/contentful/5j6wpslh72e4/zLNryDMamecpB7v
 img_cotton_candy = "https://www.wearfigs.com/i/contentful/5j6wpslh72e4/5aUA3vKIrHRMqTyUKxkg4U/8543c26b92d7be6890ebe3d2112def89/HP_Color_Carousel_288x288__MAUVE.jpg?fm=webp&w=180"
 img_azul_cielo = "https://www.wearfigs.com/i/contentful/5j6wpslh72e4/3QE3oKMDCz3gHLcMaeM3RH/d7ba5ad4694d14b905ba9449d04beb35/HP_Color_Carousel_288x288__CEIL_BLUE.jpg?fm=webp&w=180"
 img_grafito = "https://www.wearfigs.com/i/contentful/5j6wpslh72e4/5mznRvwmEt2pGPEIzcLpxt/0b3720e587c9c83c81ae6dc56b48d0d8/HP_Color_Carousel_288x288__GRAPHITE.jpg?fm=webp&w=180"
+img_rojo = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_PopRed.jpg?v=1737058532&width=180"
+img_verde_bandera = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_EVERGREEN.jpg?v=1731025370&width=180"
+img_rosa = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_Pink_Cloud.jpg?v=1733509841&width=180"
+img_naranja ="https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_ShopByColor_DIRTYCHAI.jpg?v=1727998762&width=180"
+img_mandarina = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_e0878234-382d-45a6-bb63-3b35a2f5ae00.jpg?v=1723832529&width=180"
+img_amarillo = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/288x288_ShopByColor_EmergencyYellow.jpg?v=1730308977&width=180"
+img_cacao = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/EARTH.jpg?v=1713465976&width=180"
+img_blanc = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/HP_Color_Carousel_288x288__Optic_White.jpg?v=1721182206&width=180"
+img_azul_arrecife = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/DEEP_REEF.jpg?v=1720025280&width=180"
+img_verde_pasto = "https://www.wearfigs.com/i/shopify/s/files/1/0139/8942/files/Hunter_Green.jpg?v=1711666038&width=180"
+
 
 # Estilo personalizado
 st.markdown(
     """
     <style>
         .stApp {
+            background-color: #F5F5F5;
             padding: 20px;
             border-radius: 20px;
             margin: 15px;
         }
         .title {
             font-size: 30px;
-            color: #00796b;
+            color: #028174;
             text-align: center;
             font-weight: bold;
         }
         .subtitle {
             font-size: 20px;
-            color: #004d40;
+            color: #026a52;
             text-align: center;
         }
         .topic {
             font-size: 18px;
-            color: #00796b;
+            color: #028174;
             font-weight: bold;
             margin-bottom: 10px;
         }
@@ -45,20 +57,32 @@ st.markdown(
             font-size: 15px;
             font-weight: bold;
         }
-        div.stButton > button {
-            background-color: #00796b;
-            color: white;
-            border-radius: 10px;
-            border: 2px solid #004d40;
-            font-size: 16px;
-            padding: 10px 20px;
+        .button-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100px;
         }
+        div.stButton > button {
+            background-color: #028174;
+            color: white;
+            font-size: 18px;
+            font-weight: bold;
+            padding: 15px 30px;
+            border: none;
+            border-radius: 10px;
+            cursor: pointer;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            display: block;
+            margin: auto;
+        }
+        
         div.stButton > button:hover {
-            background-color: #004d40;
+            background-color: #026a52;
             color: white;
         }
         div.stButton > button:focus {
-            background-color: #00796b !important;
+            background-color: #028174 !important;
             color: white !important;
         }
     </style>
@@ -80,13 +104,14 @@ Tu opinión será clave para crear productos únicos, prácticos y personalizado
 st.markdown('<div class="subtitle">¡Participa en nuestra encuesta y sé parte del cambio!</div>', unsafe_allow_html=True)
 
 # Botón de inicio
-if st.button("Comenzar"):
+if st.button("🚀 Comenzar Encuesta"):
     st.session_state.mostrar_encuesta = True
 
 if st.session_state.mostrar_encuesta:
     st.write("---")
+
         
-    # Perfil del participante
+    # Perfil del participante    
     with st.expander("**1. Perfil del Participante**", expanded=True):
         area = st.selectbox("¿A qué área de la salud te dedicas o estás estudiando?", [
             "Medicina General", "Especialidad Médica", "Enfermería", "Anestesiología", 
@@ -113,7 +138,7 @@ if st.session_state.mostrar_encuesta:
         ])
 
         if frecuencia == "Nunca":
-            st.markdown('<div class="subtitle">¡Gracias por tu tiempo! No necesitas responder más preguntas.</div>',
+            st.markdown('<div class="subtitle">¡Apreciamos tu tiempo! No necesitas responder más preguntas, pero si crees que esta encuesta puede ser útil para alguien en tu círculo, nos encantaría que la compartas. ¡Gracias por ayudarnos a llegar más lejos!</div>',
                          unsafe_allow_html=True)
             st.stop()
         else:
@@ -124,7 +149,7 @@ if st.session_state.mostrar_encuesta:
         identify = st.selectbox("Cuando piensas en uniforme médico, ¿qué marca es la primera que se te viene a la mente?", [
             "Skechers", "Grey's Anatomy", "Cherokee", "Figs", "Clinic Dress", 
             "Med Couture", "Healing Hands", "Dickies Medical", "Barco"
-        ], index=0)
+        ], index=0, help="Elige la que usarías más si nada te lo impidiera")
 
         most = st.radio("¿Qué consideras más importante en pijamas quirúrgicas?", [
             "Precio", "Comodidad para largas horas de trabajo", "Diseño atractivo", 
@@ -139,7 +164,8 @@ if st.session_state.mostrar_encuesta:
                 "Comodidad", "Durabilidad", "Diseño moderno", 
                 "Tejidos tecnológicos (antibacteriales, regulación térmica)", 
                 "Personalización (nombre, especialidad, etc.)"
-            ]
+            ],
+            placeholder="Elige todas las que consideres"
         )
 
         if "Tejidos tecnológicos (antibacteriales, regulación térmica)" in caracteristicas:
@@ -154,43 +180,36 @@ if st.session_state.mostrar_encuesta:
         if "selected_colors" not in st.session_state:
             st.session_state.selected_colors = []
 
-        st.markdown('<div class="topic">¿Cuál es tu top 3 de colores favoritos?</div>', unsafe_allow_html=True)
+        st.markdown('<div class="question">¿Cuál es tu top 3 de colores favoritos?</div>', unsafe_allow_html=True)
 
         # Mostrar las imágenes para seleccionar
         selected_color = image_select(
             "", 
             images=[
-                img_negro, 
-                img_azul_marino, 
-                img_azul_rey, 
-                img_carbon, 
-                img_musgo, 
-                img_malbec, 
-                img_cotton_candy, 
-                img_azul_cielo, 
-                img_grafito
+                img_blanc, img_grafito, img_carbon, img_negro, 
+                img_azul_arrecife, img_azul_marino, img_azul_rey, img_azul_cielo, 
+                img_verde_pasto, img_verde_bandera, img_musgo,
+                img_rojo, img_malbec, img_cotton_candy, img_rosa,
+                img_cacao, img_naranja, img_mandarina, img_amarillo                
             ],
             captions=[
-                "Negro", 
-                "Azul Marino", 
-                "Azul Rey", 
-                "Carbón", 
-                "Verde Musgo", 
-                "Malbec", 
-                "Cotton Candy", 
-                "Azul Cielo", 
-                "Grafito"
+                "Blanco", "Grafito", "Carbón", "Negro", 
+                "Azul Arrecife", "Azul Marino", "Azul Rey", "Azul Cielo", 
+                "Verde Pasto", "Verde Bandera", "Verde Musgo", 
+                "Rojo", "Malbec", "Cotton Candy", "Rosa",
+                "Cacao", "Naranja", "Mandarina", "Yellow"                 
             ],
             key="color_selection"
         )
 
         # Agregar el color seleccionado al listado si no excede de 3
         def add_color_to_selection(color):
-            if color and color not in st.session_state.selected_colors:
-                if len(st.session_state.selected_colors) < 3:
-                    st.session_state.selected_colors.append(color)
-                else:
-                    st.warning("Solo puedes seleccionar hasta 3 colores. Si deseas cambiar tu selección, reinicia la app.")
+            if color:
+                if color in st.session_state.selected_colors:
+                    return
+                if len(st.session_state.selected_colors) >= 3:
+                    st.session_state.selected_colors.pop(0)  # Elimina el primero automáticamente
+                st.session_state.selected_colors.append(color)
 
         if selected_color:
             add_color_to_selection(selected_color)
@@ -202,6 +221,11 @@ if st.session_state.mostrar_encuesta:
             for i, color in enumerate(st.session_state.selected_colors):
                 with cols[i]:
                     st.image(color, width=100)
+
+        # Botón para limpiar la selección
+        if st.button("Limpiar selección"):
+            st.session_state.selected_colors = []
+            st.success("¡Puedes volver a elegir tus colores favoritos!")
 
 
         # Cantidad y frecuencia de compra
@@ -223,19 +247,19 @@ if st.session_state.mostrar_encuesta:
             "Menos de \$500", "\$500 - \$700", "\$700 - \$1,000", "Más de \$1,000"
         ])
 
-        ## Interés en el modelo de suscripción
-    with st.expander("**4. Interés en el Modelo de Suscripción**", expanded=True):
         interes_suscripcion = st.select_slider(
-            "¿Qué tan interesad@ estarías en un modelo de suscripción que te envíe pijamas quirúrgicas periódicamente?",
+            f"¿Qué tan interesad@ estarías en un modelo de suscripción que te envíe pijamas quirúrgicas {frecuencia_compra}?",
             options=["No me interesa", "Poco interesad@", "Interesad@", "Muy interesad@"]
         )
+
+        ## Interés en el modelo de suscripción
+    with st.expander("**4. Colaboración y Embajadores**", expanded=True):      
         
 
         # Embajadores
-        st.markdown('<div class="topic">5. Colaboración y Embajadores</div>', unsafe_allow_html=True)
         embajador = st.radio("¿Te gustaría ser embajador de nuestra marca?", [
-            "Sí",
             "No",
+            "Sí",
             "Tal vez",
         ])
         if embajador in ["Sí", "Tal vez"]:
@@ -243,35 +267,27 @@ if st.session_state.mostrar_encuesta:
             name = st.text_input("", placeholder="Nombre y Apellido(s):")
             mail = st.text_input("", placeholder="Correo:")
             if mail and "@" not in mail:
-                st.error("Por favor, introduce un correo válido")
+                st.warning("Formato incorrecto. Usa: usuario@correo.com")
             phone = st.text_input("", placeholder="Teléfono:")
             if phone and not phone.isdigit():
-                st.error("Por favor, introduce solo números en el campo de teléfono.")
+                st.warning("Solo números son permitidos")
             redes_sociales = st.text_input("", placeholder="Redes sociales:")
             if redes_sociales and "@" not in redes_sociales:
-                st.error("Por favor, incluye '@' en tus redes sociales (por ejemplo, @usuario).")
+                st.warning("Por favor, incluye '@' en tus redes sociales (por ejemplo, @usuario).")
 
             beneficios = st.text_area(
                 "¿Qué beneficios esperarías al ser embajador?"
             )
 
-        # Agradecimiento
-        st.write("---")
-        st.write(
-            """<div class="subtitle">¡Gracias por participar! Tu opinión es muy valiosa para nosotros.</div>""", 
-            unsafe_allow_html=True)
-        
+            # Leyenda de protección de datos
+        st.markdown("""<div style='font-size: 12px; color: gray;'>
+        *La información proporcionada será utilizada exclusivamente para fines de contacto y promoción, respetando la normativa vigente en protección de datos personales.*
+        </div>""", unsafe_allow_html=True)
 
+        # Botón para enviar la encuesta
+    confirmacion = st.checkbox("Confirmo que mis respuestas son correctas")
+    if confirmacion and st.button("📩 Enviar respuestas"):
+        st.success("¡Gracias por completar la encuesta! Tu opinión es muy valiosa para nosotros.")
+        st.balloons()
 
-
-
-
-#st.checkbox('yes')
-#st.radio('Pick your gender',['Male','Female'])
-
-
-
-#st.selectbox('Pick your gender',['Male','Female'])
-#st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-#st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-#st.slider('Pick a number', 0,50)
+                
